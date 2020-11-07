e0c1bd1eafd5ebde5d070f49caa9f0f6bd312653
Xavier Capaldi <xcapaldi@scribo.biz>
2020-11-07 15:40:39 -0500

    Add function for generating initial chain.

--- src/index.js
+++ src/index.js
@@ -7,25 +7,34 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var vertices = [
-    new THREE.Vector3( 0, 10, - 10 ), new THREE.Vector3( 10, 0, - 10 ),
-    new THREE.Vector3( 20, 0, 0 ), new THREE.Vector3( 30, 0, 10 ),
-    new THREE.Vector3( 30, 0, 20 ), new THREE.Vector3( 20, 0, 30 ),
-    new THREE.Vector3( 10, 0, 30 ), new THREE.Vector3( 0, 0, 30 ),
-    new THREE.Vector3( - 10, 10, 30 ), new THREE.Vector3( - 10, 20, 30 ),
-    new THREE.Vector3( 0, 30, 30 ), new THREE.Vector3( 10, 30, 30 ),
-    new THREE.Vector3( 20, 30, 15 ), new THREE.Vector3( 10, 30, 10 ),
-    new THREE.Vector3( 0, 30, 10 ), new THREE.Vector3( - 10, 20, 10 ),
-    new THREE.Vector3( - 10, 10, 10 ), new THREE.Vector3( 0, 0, 10 ),
-    new THREE.Vector3( 10, - 10, 10 ), new THREE.Vector3( 20, - 15, 10 ),
-    new THREE.Vector3( 30, - 15, 10 ), new THREE.Vector3( 40, - 15, 10 ),
-    new THREE.Vector3( 50, - 15, 10 ), new THREE.Vector3( 60, 0, 10 ),
-    new THREE.Vector3( 70, 0, 0 ), new THREE.Vector3( 80, 0, 0 ),
-    new THREE.Vector3( 90, 0, 0 ), new THREE.Vector3( 100, 0, 0 )
-]
+//var vertices = [
+//    new THREE.Vector3( 0, 10, - 10 ), new THREE.Vector3( 10, 0, - 10 ),
+//    new THREE.Vector3( 20, 0, 0 ), new THREE.Vector3( 30, 0, 10 ),
+//    new THREE.Vector3( 30, 0, 20 ), new THREE.Vector3( 20, 0, 30 ),
+//    new THREE.Vector3( 10, 0, 30 ), new THREE.Vector3( 0, 0, 30 ),
+//    new THREE.Vector3( - 10, 10, 30 ), new THREE.Vector3( - 10, 20, 30 ),
+//    new THREE.Vector3( 0, 30, 30 ), new THREE.Vector3( 10, 30, 30 ),
+//    new THREE.Vector3( 20, 30, 15 ), new THREE.Vector3( 10, 30, 10 ),
+//    new THREE.Vector3( 0, 30, 10 ), new THREE.Vector3( - 10, 20, 10 ),
+//    new THREE.Vector3( - 10, 10, 10 ), new THREE.Vector3( 0, 0, 10 ),
+//    new THREE.Vector3( 10, - 10, 10 ), new THREE.Vector3( 20, - 15, 10 ),
+//    new THREE.Vector3( 30, - 15, 10 ), new THREE.Vector3( 40, - 15, 10 ),
+//    new THREE.Vector3( 50, - 15, 10 ), new THREE.Vector3( 60, 0, 10 ),
+//    new THREE.Vector3( 70, 0, 0 ), new THREE.Vector3( 80, 0, 0 ),
+//    new THREE.Vector3( 90, 0, 0 ), new THREE.Vector3( 100, 0, 0 )
+//]
 
-// Add a new node to the chain
-//vertices.push(new THREE.Vector3(0, 1, 5))
+function generateVertices(n) {
+    var list = [ ];
+    var i;
+    for (i = 0; i < n; i++) {
+        list.push(new THREE.Vector3(i, 0, 0));
+    };
+    return list;
+};
+
+var vertices = generateVertices(100);
+
 
 const path = new THREE.CatmullRomCurve3( vertices );
 const geometry = new THREE.TubeBufferGeometry( path, 20, 2, 8, false );
@@ -52,11 +61,16 @@ const animate = function () {
     renderer.render( scene, camera );
 };
 
+//function updateVertices(vertices) {
+//    vertices.forEach(item.set(Math.floor((Math.random() * 10) + 1),
+//                              Math.floor((Math.random() * 10) + 1),
+//                              Math.floor((Math.random() * 10) + 1)));
+//};
+        
 function updateGeometry(newPath) {
-    mesh.geometry.dispose()
-    var geometry = new THREE.TubeGeometry( newPath, 20, 2, 8, false)
-    mesh.geometry = geometry
-}
-
+    mesh.geometry.dispose();
+    var geometry = new THREE.TubeGeometry( newPath, 20, 2, 8, false);
+    mesh.geometry = geometry;
+};
 
 animate();
