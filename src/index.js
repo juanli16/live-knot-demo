70b8600e68a446355cf1d26c69e4c625f1bbea04
Justin <juan.y.li@mail.mcgill.ca>
2020-11-07 16:19:14 -0500

    refactor into functions

--- src/index.js
+++ src/index.js
@@ -1,33 +1,54 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+let scene, camera, renderer, controls;
+let geometry, material, mesh, vertices;
 
-const controls = new OrbitControls( camera, renderer.domElement );
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 20, 100 );
-controls.update();
+init();
+animate();
 
-//var vertices = [
-//    new THREE.Vector3( 0, 10, - 10 ), new THREE.Vector3( 10, 0, - 10 ),
-//    new THREE.Vector3( 20, 0, 0 ), new THREE.Vector3( 30, 0, 10 ),
-//    new THREE.Vector3( 30, 0, 20 ), new THREE.Vector3( 20, 0, 30 ),
-//    new THREE.Vector3( 10, 0, 30 ), new THREE.Vector3( 0, 0, 30 ),
-//    new THREE.Vector3( - 10, 10, 30 ), new THREE.Vector3( - 10, 20, 30 ),
-//    new THREE.Vector3( 0, 30, 30 ), new THREE.Vector3( 10, 30, 30 ),
-//    new THREE.Vector3( 20, 30, 15 ), new THREE.Vector3( 10, 30, 10 ),
-//    new THREE.Vector3( 0, 30, 10 ), new THREE.Vector3( - 10, 20, 10 ),
-//    new THREE.Vector3( - 10, 10, 10 ), new THREE.Vector3( 0, 0, 10 ),
-//    new THREE.Vector3( 10, - 10, 10 ), new THREE.Vector3( 20, - 15, 10 ),
-//    new THREE.Vector3( 30, - 15, 10 ), new THREE.Vector3( 40, - 15, 10 ),
-//    new THREE.Vector3( 50, - 15, 10 ), new THREE.Vector3( 60, 0, 10 ),
-//    new THREE.Vector3( 70, 0, 0 ), new THREE.Vector3( 80, 0, 0 ),
-//    new THREE.Vector3( 90, 0, 0 ), new THREE.Vector3( 100, 0, 0 )
-//]
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    controls = new OrbitControls( camera, renderer.domElement );
+    //controls.update() must be called after any manual changes to the camera's transform
+    camera.position.set( 0, 20, 100 );
+    controls.update();
+
+
+    vertices = generateVertices(100);
+
+    var path = new THREE.CatmullRomCurve3( vertices );
+
+    geometry = new THREE.TubeBufferGeometry( path, 20, 2, 8, false );
+    material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
+    material.side = THREE.DoubleSide;
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    scene.add( directionalLight );
+
+    camera.position.z = 50;
+}
+
+function updateVertex(item) {
+    item.set((Math.random() * 10),
+             (Math.random() * 10),
+             (Math.random() * 10));
+};
+
+function updateGeometry(newPath) {
+    mesh.geometry.dispose();
+    var geometry = new THREE.TubeGeometry( newPath, 20, 2, 8, false);
+    mesh.geometry = geometry;
+};
 
 function generateVertices(n) {
     var list = [ ];
@@ -38,24 +59,7 @@ function generateVertices(n) {
     return list;
 };
 
-var vertices = generateVertices(100);
-
-
-var path = new THREE.CatmullRomCurve3( vertices );
-const geometry = new THREE.TubeBufferGeometry( path, 20, 2, 8, false );
-const material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
-material.side = THREE.DoubleSide;
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-scene.add( directionalLight );
-
-camera.position.z = 50;
-
-const animate = function () {
+function animate() {
     // need some function here to update the vertices
     vertices.forEach(updateVertex);
     var path = new THREE.CatmullRomCurve3( vertices );
@@ -71,17 +75,3 @@ const animate = function () {
 
     renderer.render( scene, camera );
 };
-
-function updateVertex(item) {
-    item.set((Math.random() * 10),
-             (Math.random() * 10),
-             (Math.random() * 10));
-};
-        
-function updateGeometry(newPath) {
-    mesh.geometry.dispose();
-    var geometry = new THREE.TubeGeometry( newPath, 20, 2, 8, false);
-    mesh.geometry = geometry;
-};
-
-animate();
