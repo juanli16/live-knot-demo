44387f131a07b08df472a2c134725d0eec4548a2
Justin <juan.y.li@mail.mcgill.ca>
2020-11-08 10:19:29 -0500

    adding backend

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import numpy as np
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('knot')
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class Knot(Resource):
+    def post(self):
+        args = parser.parse_args()
+        knot_vertices = args['knot']
+        knot_vertices = np.array(knot_vertices)
+        print(knot_vertices)
+        return 201
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Knot, '/knot')
+
+if __name__ == '__main__':
+    app.run(debug=True)
