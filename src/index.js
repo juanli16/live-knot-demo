381e18333ed268b96236d6e142c3c6d7f3be52d4
Justin <juan.y.li@mail.mcgill.ca>
2020-11-08 10:57:55 -0500

    add post for sending coordinates to python backend

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function reqListener () {
 
 let xhr = new XMLHttpRequest();
 xhr.addEventListener("load", reqListener);
-xhr.open("POST", "127.0.0.1:5000/knot/", true);
+xhr.open("POST", "http://127.0.0.1:5000/knot", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function() {
   console.log("HELLO")
